31334151122cfdebd50cd9b45901927f83921f7a
DuckRodgers <53906002+DuckRodgers@users.noreply.github.com>
2020-09-03 23:03:22 -0300

    Add files via upload

--- app/gamefy.py
+++ app/gamefy.py
@@ -16,28 +16,34 @@ def game_maker(df_input):
         players = object['Responsible'].unique() #This obtains the players's name from the db
         count = 0
         df_res = pd.DataFrame()
-        df_half = pd.DataFrame()
-        copy = object.copy()
+        copied = object.copy()
         for player in players:
             if (count==0):
-                df_res = copy.loc[copy['Responsible'] == player]
+                df_res = copied.loc[copied['Responsible'] == player]
                 df_res['Random'] = np.random.uniform(0.0000,0.9999,size=len(df_res.index))
                 df_res = df_res.sort_values(by=['Random'])
-                df_res = df_res.iloc[:4,:]
+                df_res = df_res.iloc[:19,:]
                 count += 1
             else:
-                df_half = copy.loc[copy['Responsible'] == player]
+                df_half = pd.DataFrame()
+                df_half = copied.loc[copied['Responsible'] == player]
                 df_half['Random'] = np.random.uniform(0.0000,0.9999,size=len(df_half.index))
                 df_half = df_half.sort_values(by=['Random'])
+                df_half = df_half.iloc[:19,:]
                 list_df = [df_res,df_half]
                 res = pd.concat(list_df)
-        return res
+                df_res = res
+        for player in players:
+            df_res[str(player)] = 0
+        df_res['Random'] = np.random.uniform(0.0000,0.9999,size=len(df_res.index))
+        df_res = df_res.sort_values(by=['Random'])
+        return df_res
 
-    mid_df = refurbish_df(df_input)
+    mid_df = refurbish_df(df_input)  #I'm not sure why is this here, but it works
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     mid_df.to_excel(writer, startrow=0, merge_cells=False, sheet_name="Game")
     workbook = writer.book
     writer.close()
-    output.seek(0)
+    output.seek(0) #I'm not sure what this does
     return output
