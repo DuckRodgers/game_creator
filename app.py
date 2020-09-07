18d95b3ddcabf6d55591aca8d3e26668ec49244d
DuckRodgers <53906002+DuckRodgers@users.noreply.github.com>
2020-09-07 18:44:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd
 from gamefy import game_maker #Locally created code to create the games
 import os
 
-port = int(os.environ.get('PORT', 5000))
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,4 +22,5 @@ def upload():
 port = int(os.environ.get('PORT','5000'))
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0',port=port,debug=True)
